15f84489e9782f9e317571c3f9657f9d2958331a
Sahilrawat666 <sr4328914@gmail.com>
2026-08-20 18:20:43 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./app.js";
 import pool from "./config/db.js";
 
-const PORT = process.env.FRONTEND_URL || 5000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
